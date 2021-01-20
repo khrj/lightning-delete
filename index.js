da4f94d5465a8bbb56658c62fb4804f7cfddddcb
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-20 19:17:59 +0530

    Add slash command for viewing lightning queue

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const { RTMClient } = require('@slack/rtm-api')
 const { WebClient } = require('@slack/web-api')
+const { App, ExpressReceiver } = require('@slack/bolt')
+
 const { PrismaClient } = require("@prisma/client")
-const app = require('express')()
 const prisma = new PrismaClient()
 
-const botClient = new WebClient(process.env.SLACK_BOT_TOKEN)
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET, endpoints: '/slack/events' })
+
+const app = new App({
+    receiver,
+    token: process.env.SLACK_BOT_TOKEN,
+})
+
+const onlineSymbol = ":large_green_circle:"
+const startingSymbol = ":large_orange_circle:"
+
+let onlineUsersMap = {}
 
 async function main() {
+    await app.start(process.env.PORT || 3000)
+    console.log('⚡️ Bolt app is running!')
+
+    // RTM
+
     const regexForD = /^[dD]*$/
     function listen(id, token) {
         const rtm = new RTMClient(token)
@@ -72,60 +88,81 @@ async function main() {
                     cursor: cursor
                 })
                 replies.push(...history.messages)
-                if(history.has_more) {
-                    await getNext(history.response_metadata.next_cursor)  
+                if (history.has_more) {
+                    await getNext(history.response_metadata.next_cursor)
                 }
             }
             await getNext(undefined)
             return replies
-        } 
+        }
 
         rtm.start()
     }
 
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({
+        orderBy: {
+            queuePostition: 'asc',
+        },
+    })
+
+    for (const { slackID, token } of users) {
+        onlineUsersMap[slackID] = false
+    }
 
     for (const { slackID, token } of users) {
         listen(slackID, token)
+        onlineUsersMap[slackID] = true
+        await new Promise(resolve => setTimeout(resolve, 60000))
+    }
+}
+
+app.command('/lightning-status', async ({ command, ack, client }) => {
+    await ack()
+    let build = ":zap: Lightning Delete Status (one user goes online each minute due to rate limits):\n\n"
+
+    for (const [user, online] of Object.entries(onlineUsersMap)) {
+        build += `${online ? onlineSymbol : startingSymbol} <@${user}>\n`
     }
 
-    app.get('/auth', async (req, res) => {
-        try {
-            const response = await botClient.oauth.access({
-                client_id: process.env.SLACK_CLIENT_ID,
-                client_secret: process.env.SLACK_CLIENT_SECRET,
-                code: req.query.code
-            })
-
-            await prisma.user.upsert({
-                where: { slackID: response.user_id },
-                update: { token: response.access_token },
-                create: {
-                    slackID: response.user_id,
-                    token: response.access_token
-                }
-            })
-
-            listen(response.user_id, response.access_token)
-
-            res.send('Authed successfully')
-        } catch (e) {
-            res.redirect('/')
-            console.log(e)
-        }
+    await client.chat.postEphemeral({
+        channel: command.channel_id,
+        user: command.user_id,
+        text: build
     })
+})
 
-    app.get('/', (_, res) => {
-        res.redirect("https://slack.com/oauth/authorize?client_id=2210535565.1603461050646&scope=client")
-    })
+receiver.app.get('/auth', async (req, res) => {
+    try {
+        const response = await app.client.oauth.access({
+            client_id: process.env.SLACK_CLIENT_ID,
+            client_secret: process.env.SLACK_CLIENT_SECRET,
+            code: req.query.code
+        })
 
-    app.get('/ping', (_, res) => {
-        res.send('Online')
-    })
+        await prisma.user.upsert({
+            where: { slackID: response.user_id },
+            update: { token: response.access_token },
+            create: {
+                slackID: response.user_id,
+                token: response.access_token
+            }
+        })
 
-    app.listen(3000, () => {
-        console.log('Server started ⚡️')
-    })
-}
+        listen(response.user_id, response.access_token)
+
+        res.send('Authed successfully')
+    } catch (e) {
+        res.redirect('/')
+        console.log(e)
+    }
+})
+
+receiver.app.get('/', (_, res) => {
+    res.redirect("https://slack.com/oauth/authorize?client_id=2210535565.1603461050646&scope=client")
+})
+
+receiver.app.get('/ping', (_, res) => {
+    res.send('Online')
+})
 
 main()
